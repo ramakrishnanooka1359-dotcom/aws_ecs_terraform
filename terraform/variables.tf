5baae3ef9df6cb1176492d610a25edf8bd359c99
ramakrishnanooka1359-dotcom <ramakrishnanooka1359@gmail.com>
2025-12-12 14:22:54 +0530

    Add Terraform variables for configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "container_app_name" {
+  type = string
+}
+
+variable "dockerhub_username" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
